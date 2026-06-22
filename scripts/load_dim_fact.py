e9c7ec3bac03dc74619f8aa9e1cbba3087cc7fe5
gowthaman K <gowthaman.k0411@gmail.com>
2026-06-22 18:42:54 +0530

    Added GitHub Actions CI/CD

--- scripts/load_dim_fact.py
+++ scripts/load_dim_fact.py
@@ -1,16 +1,33 @@
-import pandas as pd
 import sqlite3
-
-customers = pd.read_csv("/opt/airflow/data/customers.csv")
-products = pd.read_csv("/opt/airflow/data/products.csv")
-orders = pd.read_csv("/opt/airflow/data/orders.csv")
+import pandas as pd
 
 conn = sqlite3.connect("/opt/airflow/data/sales.db")
 
-customers.to_sql("dim_customer", conn, if_exists="replace", index=False)
-products.to_sql("dim_product", conn, if_exists="replace", index=False)
-orders.to_sql("fact_orders", conn, if_exists="replace", index=False)
+df = pd.read_csv("/opt/airflow/data/output.csv")
+
+customer_dim = df[["customer"]].drop_duplicates()
+customer_dim["customer_id"] = range(1, len(customer_dim) + 1)
+
+customer_dim.to_sql(
+    "dim_customer",
+    conn,
+    if_exists="replace",
+    index=False
+)
+
+fact_sales = df.merge(customer_dim, on="customer")
+
+fact_sales = fact_sales[
+    ["order_id", "customer_id", "amount"]
+]
+
+fact_sales.to_sql(
+    "fact_sales",
+    conn,
+    if_exists="replace",
+    index=False
+)
+
+print("Star Schema Created Successfully")
 
 conn.close()
-
-print("Star Schema Loaded Successfully")
